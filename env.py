275613ff49bd1976f7f67770f9c04faca7382372
Raffaele Galliera <gallieraraffaele@gmail.com>
2022-02-07 13:39:59 -0600

    refactor: move server to new package

--- env.py
+++ env.py
@@ -5,7 +5,7 @@ from gym import Env
 from gym.spaces import Box, Discrete
 
 from stable_baselines3.common.type_aliases import GymObs, GymStepReturn
-import python_server.congestion_control_server as cc_server
+import data_processing_server.congestion_control_server as cc_server
 
 
 class CongestionControlEnv(Env):
