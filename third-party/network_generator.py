9ef0d60d7378d252b9de927cce00c18de4628883
Raffaele Galliera <gallieraraffaele@gmail.com>
2023-05-20 19:16:36 -0500

    feat: add parallel topologies

--- third-party/network_generator.py
+++ third-party/network_generator.py
@@ -33,53 +33,61 @@ class LinuxRouter(Node):
 
 class DumbbellTopology(Topo):
     # Simple Dumbell topology example.
+    def __init__(self, seed, n_envs):
+        self.seed = seed
+        self.n_envs = n_envs
+        super().__init__()
+
 
     def build(self):
-        # Create custom topo
 
-        r0 = self.addNode('r0', cls=LinuxRouter, ip='10.0.1.254/24')
+        for i in range(self.n_envs):
+            rank = (self.seed + i) * 10
+            print(rank)
+            # Create custom topology
+            r0 = self.addNode(f'r{rank}', cls=LinuxRouter, ip=f'10.0.{rank}.254/24')
 
-        # Add switches
-        info('*** Add switches\n')
-        ls1 = self.addSwitch('ls1')
-        rs1 = self.addSwitch('rs1')
+            # Add switches
+            info('*** Add switches\n')
+            ls1 = self.addSwitch(f'ls{rank}')
+            rs1 = self.addSwitch(f'rs{rank}')
 
-        # Add hosts
-        lh1 = self.addHost('lh1',
-                           cls=Docker,
-                           ip='10.0.1.1/24',
-                           defaultRoute='via 10.0.1.254',
-                           dimage='mockets:0.1')
+            # Add hosts
+            lh1 = self.addHost(f'lh{rank}',
+                               cls=Docker,
+                               ip=f'10.0.{rank}.1/24',
+                               defaultRoute=f'via 10.0.{rank}.254',
+                               dimage='mockets:0.1')
 
-        lh2 = self.addHost('lh2',
-                           cls=Docker,
-                           ip='10.0.1.2/24',
-                           defaultRoute='via 10.0.1.254',
-                           dimage='mgen:0.1')
+            lh2 = self.addHost(f'lh{rank + 1}',
+                               cls=Docker,
+                               ip=f'10.0.{rank}.2/24',
+                               defaultRoute=f'via 10.0.{rank}.254',
+                               dimage='mgen:0.1')
 
-        rh1 = self.addHost('rh1',
-                           cls=Docker,
-                           ip='10.0.2.1/24',
-                           defaultRoute='via 10.0.2.254',
-                           dimage='mockets:0.1')
+            rh1 = self.addHost(f'rh{rank}',
+                               cls=Docker,
+                               ip=f'10.0.{rank + 1}.1/24',
+                               defaultRoute=f'via 10.0.{rank + 1}.254',
+                               dimage='mockets:0.1')
 
-        rh2 = self.addHost('rh2',
-                           cls=Docker,
-                           ip='10.0.2.2/24',
-                           defaultRoute='via 10.0.2.254',
-                           dimage='mgen:0.1')
+            rh2 = self.addHost(f'rh{rank + 1}',
+                               cls=Docker,
+                               ip=f'10.0.{rank + 1}.2/24',
+                               defaultRoute=f'via 10.0.{rank + 1}.254',
+                               dimage='mgen:0.1')
 
-        # Add links
-        info('*** Connect the switches to the router\n')
-        self.addLink(ls1, r0, intf=TCIntf, delay='50ms', bw=2,
-                     params2={'ip': '10.0.1.254/24'})
-        self.addLink(rs1, r0, intf=TCIntf, params2={'ip': '10.0.2.254/24'})
+            # Add links
+            info('*** Connect the switches to the router\n')
+            self.addLink(ls1, r0, intf=TCIntf, delay='50ms', bw=2,
+                         params2={'ip': f'10.0.{rank}.254/24'})
+            self.addLink(rs1, r0, intf=TCIntf, params2={'ip': f'10.0.{rank + 1}.254/24'})
 
-        info("*** Connect the switches to the hosts\n")
-        self.addLink(lh1, ls1, intf=TCIntf)
-        self.addLink(lh2, ls1, intf=TCIntf)
-        self.addLink(rh1, rs1, intf=TCIntf)
-        self.addLink(rh2, rs1, intf=TCIntf)
+            info("*** Connect the switches to the hosts\n")
+            self.addLink(lh1, ls1, intf=TCIntf)
+            self.addLink(lh2, ls1, intf=TCIntf)
+            self.addLink(rh1, rs1, intf=TCIntf)
+            self.addLink(rh2, rs1, intf=TCIntf)
 
 class MininetService(rpyc.Service):
     def __init__(self, mininet):
@@ -99,9 +107,14 @@ class MininetService(rpyc.Service):
 
         return links[0][0], links[0][1]
 
-    def exposed_update_link(self, delay=None, bandwidth=None, loss=None):
+    def exposed_update_link(self,
+                            delay=None,
+                            bandwidth=None,
+                            loss=None,
+                            node_1='ls1',
+                            node_2='r0'):
         # Update latency of the shared link
-        src_link, dst_link = self.get_links()
+        src_link, dst_link = self.get_links(node_1=node_1, node_2=node_2)
 
         src_link.config(delay=delay, bw=bandwidth, loss=loss)
         dst_link.config(delay=delay, bw=bandwidth, loss=loss)
@@ -109,7 +122,7 @@ class MininetService(rpyc.Service):
         return f'Changed link to {delay} {bandwidth}Mbit {loss}%'
 
 if __name__ == '__main__':
-    topo = DumbbellTopology()
+    topo = DumbbellTopology(seed=9, n_envs=3)
     net = Mininet(topo=topo, controller=Controller, waitConnected=True)
     net.start()
 
